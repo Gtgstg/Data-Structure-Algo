1264ba3704344fd5f495c8b818e035d382637d72
Gtgstg <36958463+Gtgstg@users.noreply.github.com>
2018-10-31 02:11:28 +0530

    Add files via upload

---
+++ Seive.java
@@ -0,0 +1,33 @@
+package com.company.seive;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Main
+{  static boolean a[];
+    public static void seive(){
+        a=new boolean[10001];
+        for(int i=2;i*i<=10000;i++){
+            if(a[i]==false){
+                for(int j=i+i;j<=10000;j+=i){
+                    a[j]=true;
+                }
+            }
+        }
+    }
+    public static void main (String[] args)
+    {
+        seive();
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int j=0;j<t;j++){
+            int n=sc.nextInt();
+            for(int i=2;i<n/2.0;i++){
+                if(a[i]==false && a[n-i]==false){
+                    System.out.println(i+" "+(n-i));
+                    break;
+                }
+            }
+        }
+    }
+}
